c8987e5e7daaae59d8f4dbf905c2103143b1a2d4
kKATO_lab <di.aps53@gmail.com>
2016-09-18 15:42:04 +0900

    モジュール ML の初期チェックイン

---
+++ ML/MainClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using CsvHelper;
+
+namespace ML
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var data = LoadData.LoadCsv<Iris,IrisMap>("iris.csv");
+            foreach (var item in data.Where(x => x.Species == "setosa"))
+            {
+                Console.WriteLine("{0}\t{1}\t{2}\t{3}\t{4}",item.PetalLength,item.PetalWidth,item.SepalLength,item.SepalWidth,item.Species);
+            }
+        }
+    }
+}
---
+++ ML/SVM.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Text;
+using System.IO;
+using System.Linq;
+using Accord.MachineLearning.VectorMachines;
+using Accord.MachineLearning.VectorMachines.Learning;
+using Accord.Math;
+using Accord.IO;
+
+namespace ML
+{
+    class SVMTrainData
+    {
+        public double[][] train;
+        public int[] label;
+        public SVMTrainData()
+        {
+            Random rnd = new Random();
+            train = new double[][] 
+                {
+                    new double[] {rnd.Next(0,5),rnd.Next(0,5)},
+                    new double[] {rnd.Next(0,5),rnd.Next(0,5)},
+                    new double[] {rnd.Next(0,5),rnd.Next(0,5)},
+                    new double[] {rnd.Next(0,5),rnd.Next(0,5)},
+                    new double[] {rnd.Next(5,10),rnd.Next(5,10)},
+                    new double[] {rnd.Next(5,10),rnd.Next(5,10)},
+                    new double[] {rnd.Next(5,10),rnd.Next(5,10)},
+                    new double[] {rnd.Next(5,10),rnd.Next(5,10)},
+                };
+            label = new int[] { -1,-1,-1,-1, 1, 1, 1, 1 };
+        }
+    }
+
+    class SVM : SVMTrainData
+    {
+        public void Learn()
+        {
+            SupportVectorMachine svm = new SupportVectorMachine(train[0].Length);
+            var teacher = new LinearCoordinateDescent(svm, train, label);
+
+            double error = teacher.Run();
+
+            var answers = svm.Compute(train[0]);
+            Console.WriteLine("{0}",answers);
+            Console.WriteLine("{0}",error);
+        }
+    }
+}
+
